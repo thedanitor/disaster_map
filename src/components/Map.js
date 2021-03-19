6287be3c51479ba8f05099aa4ba2b9df81dfe2d0
Dan Weikart <danmweikart@gmail.com>
2021-03-18 21:49:21 -0700

    added map and fire icon, default to Seattle

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,28 @@
 import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
 
 const Map = ({ center, zoom }) => {
-    return (
-        <div className="map">
-            <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            defaultCenter={ center }
-            defaultZoom={ zoom }
-            >
-
-            </GoogleMapReact>
-        </div>
-    )
-}
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <LocationMarker lat={center.lat} lng={center.lng} />
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 Map.defaultProps = {
-    // defaults to Seattle
-    center: {
-        lat: 47.6062,
-        long: -122.3321,
-    },
-    // zoom level 6
-    zoom: 6,
-}
+  // defaults to Seattle
+  center: {
+    lat: 47.6062,
+    lng: -122.3321,
+  },
+  // zoom level 6
+  zoom: 6,
+};
 
-export default Map
+export default Map;
