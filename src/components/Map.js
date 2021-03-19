92ff085d60978bd64cc63d9eb0af2671a33a6ae6
Dan Weikart <danmweikart@gmail.com>
2021-03-18 22:31:54 -0700

    completed fire data

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
+import LocationInfoBox from "./LocationInfoBox";
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData, center, zoom }) => {
+  const [locationInfo, setLocationInfo] = useState(null);
+
+  // create marker for every event with ID of 8 (wildfire) at coordinates
+  const markers = eventData.map(ev => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <LocationMarker
+          lat={ev.geometries[0].coordinates[1]}
+          lng={ev.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
+        />
+      );
+    }
+    return null;
+  });
+
   return (
     <div className="map">
       <GoogleMapReact
@@ -9,8 +27,10 @@ const Map = ({ center, zoom }) => {
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        <LocationMarker lat={center.lat} lng={center.lng} />
+        {markers}
       </GoogleMapReact>
+      {/* if locationInfo is not null, then show LocationInfoBox  */}
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
 };
