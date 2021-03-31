83ead90e58c9ad6f36c20966b60dc107858a6063
Dan Weikart <danmweikart@gmail.com>
2021-03-31 11:57:00 -0700

    added minimize to selector

--- src/components/DisasterSelector.js
+++ src/components/DisasterSelector.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import disasterTypes from "../utils/DisasterTypes";
 import DisasterButtons from "./DisasterButtons";
+// import { Icon, InlineIcon } from '@iconify/react';
+// import windowMinimize from '@iconify-icons/fa-solid/window-minimize';
+
 
 const DisasterSelector = ({ handleSelectClick }) => {
+  const [minimize, setMinimize] = useState("selector");
+
+  const handleMinimize = () => {
+    minimize === "selector" ? setMinimize("selector minimize") : setMinimize("selector");
+    console.log(minimize);
+  }
+
   // map buttons for each disasterType to DisasterButtons
   const disasterButtons = disasterTypes.map(disaster => {
     return (
@@ -15,13 +26,16 @@ const DisasterSelector = ({ handleSelectClick }) => {
   });
 
   return (
-    <div className="selector">
+    <div className={minimize}>
+      {/* <Icon icon={windowMinimize} className="minimize" /> */}
+      <button className="disaster-button minimize-btn" onClick={handleMinimize}>-</button>
       <h3>Filter by</h3>
       {/* Button for all disasters, sets typeFilter to blank */}
       <button className="disaster-button" onClick={handleSelectClick} value="all">
         All Disasters
       </button>
       {disasterButtons}
+      <button className="disaster-button" onClick={handleSelectClick} value="">Clear Disasters</button>
     </div>
   );
 };
