26dc6ba5aca9371608097674a33ccd5b481d7ebd
Dan Weikart <danmweikart@gmail.com>
2021-04-09 10:34:34 -0700

    reinstalled all iconify icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
       <footer className="footer">
-        <h5>Copyright &copy; <a href="https://github.com/thedanitor">Dan Weikart</a> All Rights Reserved </h5>
+        <h5>Copyright &copy; <a href="https://github.com/thedanitor" target="_blank" rel="noreferrer">Dan Weikart</a> All Rights Reserved </h5>
       </footer>
     );
   };
